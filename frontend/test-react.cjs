c135073105ee4c678fbe8c6b9a60971cf5bd3dc9
Pawan Prajapati <pavnkumarprajapati2000@gmail.com>
2026-05-18 22:47:02 +0530

    Chore: Remove debug scripts

--- frontend/test-react.cjs
+++
@@ -1,9 +0,0 @@
-const { createElement } = require('react');
-const { renderToStaticMarkup } = require('react-dom/server');
-const QRCode = require('react-qr-code').default;
-
-try {
-  console.log(renderToStaticMarkup(createElement(QRCode, { value: undefined, size: 180 })));
-} catch (e) {
-  console.error("CRASHED:", e);
-}
